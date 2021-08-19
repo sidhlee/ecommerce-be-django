9b586abb1420f4cff7e511d351416ffb8993b1da
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-19 19:45:16 -0400

    start base app and register

---
+++ ecomm_backend/base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def getRoute(request):
+  routes = [
+    '/api/products',
+    '/api/products/create',
+
+    '/api/products/upload/',
+
+    '/api/products/<id>/reviews/',
+
+    '/spi/products/top/',
+    '/api/products/<id>/',
+
+    '/api/products/delete/<id>',
+    '/api/products/<update>/<id>/',
+  ]
+
+  # If safe=True (default) only dictionary is allowed to be serialized
+  return JsonResponse(routes, safe=False)
