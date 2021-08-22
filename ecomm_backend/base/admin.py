f70d3c9c7cd97daa7725845559c4fec0d9259cb8
Sid Hayoun Lee <sidhlee@gmail.com>
2021-08-21 23:12:52 -0400

    add Order, OrderItem, and ShippingAddress models

--- ecomm_backend/base/admin.py
+++ ecomm_backend/base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variant
+from .models import Order, OrderItem, Product, ShippingAddress, Variant
 
 # Register your models here.
-admin.site.register([Product, Variant])
+admin.site.register([Product, Variant, Order, OrderItem, ShippingAddress ])
--- ecomm_backend/base/models.py
+++ ecomm_backend/base/models.py
@@ -8,7 +8,10 @@ class Product(models.Model):
   name = models.CharField(max_length=200, null=False, blank=False)
   image_url = models.URLField(max_length=200)
   likes = likes = models.PositiveIntegerField(default=0)
-  created = models.DateTimeField(auto_now_add=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return str(f'{self.product_id}_{self.name}')
 
 # Will have foreignField in "many" side
 class Variant(models.Model):  
@@ -23,4 +26,41 @@ class Variant(models.Model):
   currency = models.CharField(max_length=10, null=False, blank=False, default="CAD")
   image_url = models.URLField(max_length=200)
   likes = models.PositiveIntegerField(default=0)
-  created = models.DateTimeField(auto_now_add=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return str(f'{self.variant_id}_{self.name}')
+
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  payment_method = models.CharField(max_length=200, null=False, blank=False)
+  tax_price = models.DecimalField(max_digits=7, decimal_places=2) 
+  shipping_price = models.DecimalField(max_digits=7, decimal_places=2) 
+  total_price = models.DecimalField(max_digits=7, decimal_places=2) 
+  is_paid = models.BooleanField(default=False)
+  paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+  is_delivered = models.BooleanField(default=False)
+  delivered_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return str(self.created_at)
+  
+class OrderItem(models.Model):
+  variant = models.ForeignKey(Variant, on_delete=models.SET_NULL, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)  
+  qty = models.PositiveIntegerField(null=True, blank=True, default = 0)
+
+  def __str__(self):
+    return str(self.variant.name)  
+
+class ShippingAddress(models.Model):
+  order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+  address = models.CharField(max_length=200)
+  city = models.CharField(max_length=20)
+  postalcode = models.CharField(max_length=20)
+  country = models.CharField(max_length=20)
+  shipping_price = models.DecimalField(max_digits=7, decimal_places=2)
+
+  def __str__(self):
+    return str(self.address)
